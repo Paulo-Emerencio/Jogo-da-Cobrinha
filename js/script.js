c986fad8639c8c2fcb10ef717779fbba133138c1
Paulo Emerencio <p.emerencio@gmail.com>
2021-03-31 11:56:45 -0300

    :rocket: snake movement

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
+// Background
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+// A cobrinha
 let snake = [];
 snake[0] = {
     x: 8 * box,
     y: 8 * box,
 }
+// Direção inicial da cobrinha
+let direction = "right";
 
 function createBG() {
     context.fillStyle = "lightgreen";
@@ -19,5 +23,24 @@ function createSnake() {
     }
 }
 
-createBG();
-createSnake();
+function startGame() {
+    createBG();
+    createSnake();
+    // Coordenadas da cobrinha
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
+    // Removendo a posição anterior do Array quando a cobrinha anda
+    snake.pop();
+    // Adicionando uma nova posição no Array para fazer a cobrinha andar
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+    snake.unshift(newHead);
+}
+let game = setInterval(startGame, 100);
+
