60537a76766c0f8b774de4f1cc5d7ca367566663
Paulo Emerencio <p.emerencio@gmail.com>
2021-04-01 10:32:47 -0300

    :zap: Defining the keys and the boundaries of canvas for the snake

--- js/script.js
+++ js/script.js
@@ -23,7 +23,21 @@ function createSnake() {
     }
 }
 
+document.addEventListener('keydown', update);
+function update(event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
 function startGame() {
+    // Resetando a posição da cobrinha quando ela passa as bordas
+    if (snake[0].x > 15 * box) snake[0].x = 0;
+    if (snake[0].x < 0) snake[0].x = 15 * box;
+    if (snake[0].y > 15 * box) snake[0].y = 0;
+    if (snake[0].y < 0) snake[0].y = 15 * box;
+
     createBG();
     createSnake();
     // Coordenadas da cobrinha
@@ -35,12 +49,12 @@ function startGame() {
     if (direction == "down") snakeY += box;
     // Removendo a posição anterior do Array quando a cobrinha anda
     snake.pop();
-    // Adicionando uma nova posição no Array para fazer a cobrinha andar
-    let newHead = {
+    // Adicionando uma nova posição no começo do Array para fazer a cobrinha andar
+    let newPosition = {
         x: snakeX,
         y: snakeY,
     }
-    snake.unshift(newHead);
+    snake.unshift(newPosition);
 }
 let game = setInterval(startGame, 100);
 
