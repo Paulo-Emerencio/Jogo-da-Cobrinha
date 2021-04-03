4a32517c457bdfb45fe3ce11484193f622355a1d
Paulo Emerencio <p.emerencio@gmail.com>
2021-04-03 17:22:19 -0300

    :bug: Arrumando o bug da comida na posição zero e a cobra que não pega dando a volta no canvas

--- js/script.js
+++ js/script.js
@@ -42,9 +42,9 @@ function newDirection(event) {
 function startGame() {
     // Resetando a posição da cobrinha quando ela passa as bordas
     if (snake[0].x > 15 * box) snake[0].x = 0;
-    if (snake[0].x < 0) snake[0].x = 15 * box;
+    if (snake[0].x < 0) snake[0].x = 16 * box;
     if (snake[0].y > 15 * box) snake[0].y = 0;
-    if (snake[0].y < 0) snake[0].y = 15 * box;
+    if (snake[0].y < 0) snake[0].y = 16 * box;
     // Executando as funções que desenham os elementos do jogo
     createBG();
     createSnake();
@@ -58,8 +58,20 @@ function startGame() {
     if (direction == "down") snakeY += box;
 
     if (snakeX != food.x || snakeY != food.y) {
-        // Removendo a última posição do Array
-        snake.pop();
+        /* 
+        Gambiarra de IFs para consertar um bug no código original da professora Gabriela Pinheiro.
+        Quando a comida aparecia na posição zero de X ou Y, a cobra era incapaz de pegá-la saindo pelo lado oposto do Canvas e resetando sua posição onde estava a comida, porque nesses casos, o valor mínimo para as variáveis snakeX e snakeY é 32, pois logo aqui acima no código, essas variáveis que inicialmente recebem o valor zero de snake[0].x ou snake[0].y, são somadas com a variável box, então o programa entendia que snakeX ou snakeY, que agora valem 32, são diferentes de food.x ou food.y, que valem zero, executando assim o snake.pop() de dentro do if, ao invés de executar o else que reseta a posição da comida e aumenta a cobrinha.
+        Bem, espero que eu tenha conseguido me fazer entender.
+        */
+        if (food.x == 0 && snake[0].x == 0 && snake[0].y == food.y) {
+            food.x = Math.floor(Math.random() * 15 + Math.random()) * box;
+            food.y = Math.floor(Math.random() * 15 + Math.random()) * box;
+        } else if (food.y == 0 && snake[0].y == 0 && snake[0].x == food.x) {
+            food.x = Math.floor(Math.random() * 15 + Math.random()) * box;
+            food.y = Math.floor(Math.random() * 15 + Math.random()) * box;
+        } else {
+            snake.pop();
+        }
     } else {
         food.x = Math.floor(Math.random() * 15 + Math.random()) * box;
         food.y = Math.floor(Math.random() * 15 + Math.random()) * box;
